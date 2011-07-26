07a2a58334398dec29ee4399f501bdd64d1a9ed8
Lawrence McAlpin <admin@lmcalpin.com>
2011-07-25 23:19:29 -0400

    new test case for JPAIndexedRecordLocator usage

---
+++ test/play/modules/paginate/ModelPaginatorTest.java
@@ -0,0 +1,44 @@
+package play.modules.paginate;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import play.modules.paginate.locator.JPAIndexedRecordLocator;
+import play.test.UnitTest;
+
+public class ModelPaginatorTest extends UnitTest {
+    @Before
+    public void before() {
+        if (MockModel.count() == 0) {
+            for (int i = 0; i < 15; i++) {
+                MockModel model = new MockModel();
+                model.testValue = String.valueOf(i + 1);
+                model.save();
+            }
+        }
+    }
+    
+    @Test
+    public void testCount() {
+        ModelPaginator paginator = new ModelPaginator(MockModel.class);
+        Assert.assertEquals(15, paginator.size());
+    }
+
+    @Test
+    public void testOrderByDescending() {
+        ModelPaginator<MockModel> paginator = new ModelPaginator(
+                new JPAIndexedRecordLocator(MockModel.class).setOrderBy("ID DESC"));
+        Assert.assertEquals("15", paginator.get(0).testValue);
+        Assert.assertEquals("1", paginator.get(14).testValue);
+    }
+
+    @Test
+    public void testOrderByAscending() {
+        ModelPaginator<MockModel> paginator = new ModelPaginator(
+                new JPAIndexedRecordLocator(MockModel.class).setOrderBy("ID ASC"));
+        Assert.assertEquals("1", paginator.get(0).testValue);
+        Assert.assertEquals("15", paginator.get(14).testValue);
+    }
+
+}
